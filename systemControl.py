74b023a1d015993ad7b1a621e5bacc3fddff09dc
Arik Grahl <mail@arik-grahl.de>
2014-11-16 15:12:24 +0100

    python flavoured constant declaration

--- systemControl.py
+++ systemControl.py
@@ -5,8 +5,8 @@ import subprocess
 import platform
 
 # defining constants
-kUnsupportedPlatform = 'notSupported'
-kMacOSX = 'Darwin'
+UNSUPPORTED_PLATFORM = 'notSupported'
+MAC_OS_X = 'Darwin'
 
 class SystemControl:
     def __init__(self, beVerbose):
@@ -14,25 +14,25 @@ class SystemControl:
         self.beVerbose = beVerbose
 
         # define OS identification for OS dependent sleep / volume commands:
-        if kMacOSX in platform.platform():
-            self.currentOSIdentifier = kMacOSX
+        if MAC_OS_X in platform.platform():
+            self.currentOSIdentifier = MAC_OS_X
         else:
-            self.currentOSIdentifier = kUnsupportedPlatform
+            self.currentOSIdentifier = UNSUPPORTED_PLATFORM
 
     def setSleep(self):
         if self.beVerbose: print('Sleep now. Good night!')
 
-        if self.currentOSIdentifier == kMacOSX:
+        if self.currentOSIdentifier == MAC_OS_X:
             subprocess.call(['osascript', '-e', 'tell application "System Events" to sleep'])
-        elif self.currentOSIdentifier == kUnsupportedPlatform:
+        elif self.currentOSIdentifier == UNSUPPORTED_PLATFORM:
             print('sleep for this platform not yet implemented!')
 
     def setShutdown(self):
         if self.beVerbose: print('Shutdown now. Good night!')
         
-        if self.currentOSIdentifier == kMacOSX:
+        if self.currentOSIdentifier == MAC_OS_X:
             subprocess.call(['osascript', '-e', 'tell application "System Events" to shut down'])
-        elif self.currentOSIdentifier == kUnsupportedPlatform:
+        elif self.currentOSIdentifier == UNSUPPORTED_PLATFORM:
             print('shutdown for this platform not yet implemented!')
 
     def setVolume(self, percent):
@@ -43,16 +43,16 @@ class SystemControl:
             if self.beVerbose: print('setting the volume to', str(percent), 'is not possible; settint it to 0%')
             percent = 0
 
-        if self.currentOSIdentifier == kMacOSX:
+        if self.currentOSIdentifier == MAC_OS_X:
             targetVolume = (7 * percent) / 100
             subprocess.call(['osascript', '-e', 'Set volume ' + str(targetVolume)])
-        elif self.currentOSIdentifier == kUnsupportedPlatform:
+        elif self.currentOSIdentifier == UNSUPPORTED_PLATFORM:
             print('setting the volume for this platform not yet implemented!')
 
     def getVolume(self):
-        if self.currentOSIdentifier == kMacOSX:
+        if self.currentOSIdentifier == MAC_OS_X:
             # subprocess.call(['osascript', '-e', 'get volume settings']) # TODO
             return 100
-        elif self.currentOSIdentifier == kUnsupportedPlatform:
+        elif self.currentOSIdentifier == UNSUPPORTED_PLATFORM:
             print('setting the volume for this platform not yet implemented!')
             return 100
